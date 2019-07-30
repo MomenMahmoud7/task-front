7eeb90bd97ed41f3216feea49eff6384781c4294
Motaz Abu Elnasr <motaz.abuelnasr@gmail.com>
2019-07-30 17:53:46 +0200

    verfication result component

--- src/body/body.js
+++ src/body/body.js
@@ -1,42 +1,39 @@
-import React, { useContext } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { GlobalContext } from '../Context/GlobalContext';
-import Signup from '../signup/signup';
-import Signin from '../signin/signin';
-import CodeVerification from '../codeVerification/codeVerification';
-import EmailVerification from '../emailVerification/emailVerification';
-
+import React, { useContext } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { GlobalContext } from "../Context/GlobalContext";
+import Signup from "../signup/signup";
+import Signin from "../signin/signin";
+import CodeVerification from "../codeVerification/codeVerification";
+import EmailVerification from "../emailVerification/emailVerification";
+import Result from "../VerficationResult/Result";
 const Body = () => {
-    const { userStatus } = useContext(GlobalContext);
+  const { userStatus } = useContext(GlobalContext);
 
-    return (
-        <div style={{ marginTop: '128px' }}>
-            <Switch>
-                <Route
-                    exact
-                    path='/phoneverification'
-                    render={() => <CodeVerification />}
-                />
-                <Route
-                    exact
-                    path='/emailverification'
-                    render={() => <EmailVerification />}
-                />
-                <Route exact path='/signin' render={() => <Signin />} />
-                <Route
-                    exact
-                    path='/signup'
-                    render={() =>
-                        userStatus ? (
-                            <Redirect to={`/${userStatus}`} />
-                        ) : (
-                            <Signup />
-                        )
-                    }
-                />
-                )}
-            </Switch>
-        </div>
-    );
+  return (
+    <div style={{ marginTop: "128px" }}>
+      <Switch>
+        <Route
+          exact
+          path="/phoneverification"
+          render={() => <CodeVerification />}
+        />
+        <Route
+          exact
+          path="/emailverification"
+          render={() => <EmailVerification />}
+        />
+        <Route exact path="/signin" render={() => <Signin />} />
+        <Route exact path="/confirmMail" component={Result} />
+        <Route
+          exact
+          path="/signup"
+          render={() =>
+            userStatus ? <Redirect to={`/${userStatus}`} /> : <Signup />
+          }
+        />
+        )}
+      </Switch>
+    </div>
+  );
 };
 export default Body;
